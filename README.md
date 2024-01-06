5e86485797ee952646ec6a9d6954fb75f7609fa6
Bryan <39394303+taidaid@users.noreply.github.com>
2024-01-06 13:47:09 -0500

    Update README and config name

--- README.md
+++ README.md
@@ -6,9 +6,6 @@ View the [FormKit Docs](https://formkit.com) from within VS Code.
 
 To use this extension, simply use `⌘ + Shift + P` to open the command palette (`Ctrl` on Windows / Linux) and search for "FormKit Docs" or the specific topic you're after.
 
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
 
 ## Extension Settings
 
--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const config = vscode.workspace.getConfiguration('formKitDocs');
+	const config = vscode.workspace.getConfiguration('formkitDocs');
 	const commandName = config.useSimpleBrowser ? 'simpleBrowser.show' : 'vscode.open';
 
 	const inputTypes = [
